fa7e46b727dd0d9a2920061456265eb7ede6bcf7
UnixSupremacist <100294596+unix-supremacist@users.noreply.github.com>
2024-11-21 15:48:36 -0600

    add temp files

---
+++ bbbb.groovy
@@ -0,0 +1,9 @@
+	modImplementation include("maven.modrinth:quad:${quad_version}") //need for more variants
+	modImplementation include("maven.modrinth:more-bookshelf-variants-lieonlion:${more_bookshelf_variants_version}")
+	modImplementation include("maven.modrinth:more-cartography-tables:${more_cartography_tables_version}")
+	modImplementation include("maven.modrinth:more-crafting-tables-lieonlion:${more_crafting_tables_version}")
+	modImplementation include("maven.modrinth:more-fletching-tables:${more_fletching_tables_version}")
+	modImplementation include("maven.modrinth:more-smithing-tables:${more_smithing_tables_version}")
+	modImplementation include("maven.modrinth:many-flowers:${many_flowers_version}")
+	//modImplementation include("maven.modrinth:better-withered-mobs:${better_withered_mobs_version}")
+	include("maven.modrinth:elytra_trinket:${elytra_trinket_version}")
